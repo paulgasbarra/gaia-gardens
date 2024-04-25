2783c61ce333b8dd675c7da7dc8924266eacf4fc
Paul Gasbarra <paulgasbarra@gmail.com>
2024-04-24 21:01:42 -0400

    styling contacts and projects

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
 var app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -23,36 +23,9 @@ app.get("/contact", function (req, res) {
   res.render("pages/contact");
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// After your previous code in server.js
-app.post("/send", async (req, res) => {
-  console.log(req.body);
-  let { name, email, subject, message } = req.body;
-
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  let mailOptions = {
-    from: "your-email@gmail.com",
-    to: email, // or any email address you want to send to
-    subject: subject,
-    text: `Message from: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    res.send("Email sent successfully");
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).send("Failed to send email");
-  }
+app.post("/submit-form", function (req, res) {
+  console.log("sending mail to", req.body);
+  res.render("pages/success");
 });
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
