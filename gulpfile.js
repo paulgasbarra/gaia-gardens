026c379ae29db4baef56d10969832b8c595e81bd
Paul Gasbarra <paulgasbarra@gmail.com>
2024-05-10 21:53:26 -0400

    working more with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,13 @@ const clean = require("gulp-rimraf");
 
 const projects = require("./public/data/projects.json");
 
-gulp.task("clean", [], function () {
-  console.log("Clean all files in build folder");
-  return gulp.src("build/*", { read: false }).pipe(clean());
-});
-
 gulp.task("ejs", () => {
   return gulp
     .src("views/pages/**/*.ejs")
     .pipe(ejs({ projects: projects })) // Pass data to EJS
-    .pipe(replace("public/assets/images/", "/dist/images/"))
-    .pipe(replace("public/assets/scripts/", "/dist/scripts/"))
-    .pipe(replace("public/assets/css/", "/dist/styles/"))
+    .pipe(replace("/assets/images/", "/images/"))
+    .pipe(replace("/assets/scripts/", "/scripts/"))
+    .pipe(replace("/assets/css/", "/styles/"))
     .pipe(htmlmin({ collapseWhitespace: true })) // Minify HTML
     .pipe(rename({ extname: ".html" })) // Rename file extensions to .html
     .pipe(gulp.dest("dist")); // Output to dist folder
@@ -26,14 +21,21 @@ gulp.task("ejs", () => {
 
 gulp.task("scripts", function () {
   return gulp
-    .src("./public/assets/scripts") // Adjust the source directory as needed
+    .src("./public/assets/scripts/*.js") // Adjust the source directory as needed
     .pipe(gulp.dest("dist/scripts")); // Output directory
 });
 
 gulp.task("images", function () {
   return gulp
-    .src("./public/assets/images") // Adjust the source directory as needed
-    .pipe(gulp.dest("dist/images")); // Output directory
+    .src("./public/assets/images/*") // Adjust the source directory as needed
+    .pipe(gulp.dest("dist/images")) // Output directory
+    .on("error", console.error.bind(console));
 });
 
-gulp.task("default", gulp.series("clean", "ejs", "images", "scripts"));
+gulp.task("css", function () {
+  return gulp
+    .src("./public/css/*") // Adjust the source directory as needed
+    .pipe(gulp.dest("dist/styles")); // Output directory
+});
+
+gulp.task("default", gulp.series("ejs", "scripts", "images", "css"));
