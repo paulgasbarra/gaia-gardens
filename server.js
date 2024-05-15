b3dbe3e92736e72f648ee9e286cb961294017fac
Paul Gasbarra <paulgasbarra@gmail.com>
2024-05-15 11:08:14 -0400

    hooking up netflify

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 "use strict";
 import express from "express";
-import serverless from "serverless-http";
 import bodyParser from "body-parser";
 import { projects } from "./public/data/projects.js";
-import nodemailer from "nodemailer";
+//import nodemailer from "nodemailer";
 
 const app = express();
 
@@ -35,35 +34,31 @@ app.get("/contact", function (req, res) {
 });
 
 app.post("/submit-form", function (req, res) {
-  const { name, email, township, message } = req.body;
-
-  let transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  let mailOptions = {
-    from: email,
-    to: process.env.EMAIL_ADDRESS,
-    subject: `Gaia Gardens Contact Request: ${name} from ${township} has contacted you`,
-    text: `${name} writes ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending mail:", error);
-      res.render("pages/contact-error", { error: error.message });
-    } else {
-      console.log("Mail sent:", info.response);
-      res.render("pages/contact-success");
-    }
-  });
+  // const { name, email, township, message } = req.body;
+  // let transporter = nodemailer.createTransport({
+  //   host: process.env.SMTP_HOST,
+  //   port: process.env.SMTP_PORT,
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.EMAIL_ADDRESS,
+  //     pass: process.env.EMAIL_PASSWORD,
+  //   },
+  // });
+  // let mailOptions = {
+  //   from: email,
+  //   to: process.env.EMAIL_ADDRESS,
+  //   subject: `Gaia Gardens Contact Request: ${name} from ${township} has contacted you`,
+  //   text: `${name} writes ${message}`,
+  // };
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //     console.error("Error sending mail:", error);
+  //     res.render("pages/contact-error", { error: error.message });
+  //   } else {
+  //     console.log("Mail sent:", info.response);
+  //     res.render("pages/contact-success");
+  //   }
+  // });
 });
 
 export default app;
-export const handler = serverless(app);
